198f5aada1555a213cbbccc15d0f441cee1976aa
Wayline Jeffries <jeffrieswayline@gmail.com>
2023-10-31 15:39:11 +0200

    Task 1

---
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+last = number % 10
+str = f'Last digit of {number} is {last}'
+if last > 5:
+    print(str + ' and is greater than 5')
+elif last == 0:
+    print(str + ' and is 0')
+elif last < 6:
+    print(str + ' and is less than 6 and not 0')
