054d677f6b82d720a7512bfd888aec81502e6c1a
Wayline Jeffries <jeffrieswayline@gmail.com>
2023-11-02 07:52:57 +0200

    Task 12

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-for number in range(1, 100):
-    if number % 3 == 0 and number % 9 != 0:
-        print('Fizz ')
-    if number % 9 == 0 and number % 3 != 0:
-        print('Buzz ')
-    if number % 9 == 0 and number % 3 == 0:
-        print('FizzBuzz ')
+for number in range(1, 101):
+    if number % 3 == 0 and number % 5 == 0:
+        print("FizzBuzz ", end="")
+    elif number % 3 == 0:
+        print("Fizz ", end="")
+    elif number % 5 == 0:
+        print("Buzz ", end="")
     else:
-        print(f'{number}')
+        print("{} ".format(number), end="")
