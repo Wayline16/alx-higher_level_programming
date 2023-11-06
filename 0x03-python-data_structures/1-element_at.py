5c375a57fdb4d0465cf8e20c95414b5530252082
Wayline Jeffries <jeffrieswayline@gmail.com>
2023-11-06 15:14:36 +0200

    Task 1

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
     if idx < 0:
-        exit()
-    elif idx > len(my_list):
-        exit()
+        return None
+    elif idx >= len(my_list):
+        return None
     return my_list[idx]
