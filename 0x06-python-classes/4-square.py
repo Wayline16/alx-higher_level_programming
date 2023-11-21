9a9233e42245c82180bb90a0921f0640bfe42c09
Wayline Jeffries <jeffrieswayline@gmail.com>
2023-11-21 23:26:46 +0200

    4-square.py

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+""""Write an empty class Square that defines a square:"""
+"""Defines a class Square"""
+
+class Square:
+    """Class that defines properties of square."""
+    """"Private instance attribute: size"""
+    
+    
+    def __init__(self, size=0):
+        """"Instantiation with size"""
+        self.__size = size
+        
+    """"Public instance method: def area(self): that returns the current square area"""
+    def area(self):
+        """"Return: Area of a Square"""
+        return self.__size ** 2
+    
+    """"Define a Getter"""
+    @property
+    def size(self):
+        """Returns the size of a square"""
+        return self.__size
+    
+    """"Define a Setter"""
+    @size.setter
+    def size(self, value):
+        """Returns the size of a square"""
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    pass
