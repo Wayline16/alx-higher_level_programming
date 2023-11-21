87d3cf848578fd924018f8765d806352956e7ffe
Wayline Jeffries <jeffrieswayline@gmail.com>
2023-11-21 22:41:46 +0200

    0-square.py

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+""""Write an empty class Square that defines a square:"""
+"""Defines a class Square"""
+
+class Square:
+    """Class that defines properties of square."""
+    pass
