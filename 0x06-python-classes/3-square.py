9cdb37af240082acb44fa53f5b181b57a01abbd5
Wayline Jeffries <jeffrieswayline@gmail.com>
2023-11-21 23:07:40 +0200

    3-square.py

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+""""Write an empty class Square that defines a square:"""
+"""Defines a class Square"""
+
+class Square:
+    """Class that defines properties of square."""
+    """"Private instance attribute: size"""
+    
+    
+    def __init__(self, size=0):
+        """"Instantiation with size"""
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+    
+    """"Public instance method: def area(self): that returns the current square area"""
+    def area(self):
+        """"Return: Area of a Square"""
+        return self.__size ** 2
+
+    pass
