814b22b877625bb947f53274c0944fcb3487192b
Wayline Jeffries <jeffrieswayline@gmail.com>
2023-11-21 22:58:54 +0200

    2-square.py

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -7,8 +7,13 @@ class Square:
     """"Private instance attribute: size"""
     
     
-    def __init__(self, __size):
+    def __init__(self, size=0):
         """"Instantiation with size"""
-        self.__size = __size
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
 
     pass
