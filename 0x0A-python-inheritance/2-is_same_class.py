9bd47b567d9e8b926c473e7cb0ae2a55a9464ad9
Wayline Jeffries <jeffrieswayline@gmail.com>
2023-12-04 16:23:25 +0200

    Returns:

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Module containing is_same_class method"""
+def is_same_class(obj, a_class):
+    """Returns:
+    True: if the object is exactly an instance of the specified class
+    False: otherwise"""
+    return isinstance(obj, a_class) and type(obj) is a_class
