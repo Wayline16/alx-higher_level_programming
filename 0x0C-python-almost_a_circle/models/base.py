5fb48cf273fe127c7a79e773165b4587486920aa
Wayline Jeffries <jeffrieswayline@gmail.com>
2023-12-11 12:29:16 +0200

    task 15

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
+from json import dumps, loads
+import csv
 
 class Base:
     """Base class"""
@@ -12,3 +14,11 @@ class Base:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
             
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        '''Jsonifies a dictionary so it's quite rightly and longer.'''
+        if list_dictionaries is None or not list_dictionaries:
+            return "[]"
+        else:
+            return dumps(list_dictionaries)
+    
