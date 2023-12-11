40e21b7ea7692ab7f16a04a7de37a2c49792aba2
Wayline Jeffries <jeffrieswayline@gmail.com>
2023-12-11 11:33:13 +0200

    task 2 & 3

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 
 class Base:
+    """Base class"""
     __nb_objects = 0
     
     def __init__(self, id=None):
-        '''Constructor.'''
+        """Constructor"""
         if id is not None:
             self.id = id
         else:
