ea1eda71725343e1c5e3b1784331db828f39bc9c
Wayline Jeffries <jeffrieswayline@gmail.com>
2024-02-12 10:14:33 +0200

    Task 2. Arguments

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+// Print if argument is found
+
+if (process.argv.length === 2) {
+    console.log('No argument');
+  } else if (process.argv.length > 2) {
+    console.log('Argument found');
+  }
