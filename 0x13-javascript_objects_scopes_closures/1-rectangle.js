b35b7fb13de9108b9950b6ed2936f28eea0f0d24
Wayline Jeffries <jeffrieswayline@gmail.com>
2024-02-13 08:49:35 +0200

    Task 1. Rectangle #1

---
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+// Added the constructor to the Rectangle class
+class Rectangle {
+    constructor(w,h){
+        this.width = w;
+        this.height = h;
+    }
+}
+module.exports = Rectangle;
