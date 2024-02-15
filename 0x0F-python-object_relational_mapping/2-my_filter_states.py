18fe38479bc20ffc5dee521bddd6d09f990657fb
Wayline Jeffries <jeffrieswayline@gmail.com>
2024-02-15 14:05:02 +0200

    v

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,26 @@
+#!/urs/bin/python3
+"""List all the states from the database hbtn_0e_o_usa"""
+
+import MYSQLdb
+from sys import argv
+
+if __name__ == '__main__':
+    
+    # connection to the database
+    db = MYSQLdb.connect(host="localhost", port=3306, user=argv[1], passwd=argv[2], db=argv[3])
+    
+    cur = db.cursor()
+    nameSearch = "SELECT * FROM states WHERE name LIKE BINARY '{}'".format(argv[4])
+    cur.execute(nameSearch)
+    
+    # fetch all rows
+    rows = cur.fetchall()
+    
+    # loop through each individual row
+    for row in rows:
+        print(row)
+
+    # Clean up process 
+    cur.close()
+    db.close()  
+    
